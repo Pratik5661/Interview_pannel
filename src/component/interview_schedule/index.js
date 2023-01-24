cd70bfc0f74d74151588e1c5f4f2cae7980e4d10
Pratik5661 <pratikch199@gmail.com>
2023-01-24 10:28:07 +0530

    interview sechedule desgining

--- src/component/interview_schedule/index.js
+++ src/component/interview_schedule/index.js
@@ -6,14 +6,15 @@ import './style.scss'
 
 function Interview_Schedule() {
   return (
-    <div>
-        <h1>Interview Schedule</h1>
+    <div className='main'>
+        <h1 className='heading'>Interview Schedule</h1>
     <Card style={{ width: '30rem', height: '25rem' }} >
 
       <Card.Body  className='interview_schedule'>
         <Card.Title className='card_title'>Pick your time and Date</Card.Title>
       <div>
         <div className='date_picker'>
+            <p className='date'>Date</p>
       <DatePicker /> 
             <div className='hours'>
                 <p className='time'>Hours</p>
@@ -22,6 +23,14 @@ function Interview_Schedule() {
                     <option value="2">02</option>
                     <option value="3">03</option>
                     <option value="4">04</option>
+                    <option value="4">05</option>
+                    <option value="4">06</option>
+                    <option value="4">07</option>
+                    <option value="4">08</option>
+                    <option value="4">09</option>
+                    <option value="4">10</option>
+                    <option value="4">11</option>
+                    <option value="4">12</option>
                 </select>
             </div>
             <div className='mint'>
@@ -31,11 +40,13 @@ function Interview_Schedule() {
                     <option value="2">15</option>
                     <option value="3">30</option>
                     <option value="4">45</option>
+                    <option value="4">60</option>
                 </select>
             </div>
       </div>
         <div className='textarea'>
-                <textarea rows="4" cols="50" placeholder='comment..'/> 
+            <p className='message'>Enter your message :</p>
+                <textarea rows="4" cols="50" placeholder='message...'/> 
             </div>
       </div>
         <Card.Text>
